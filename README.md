18bdeb0a06fa0fae4911cd14c89cea78f3c4143a
Rafael Pulla da Rosa <97187570+RPulla@users.noreply.github.com>
2025-12-15 02:03:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Curso.dev do felipedeschamps implementaçao do tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1 > Olá Galera, rodando deploy na Vercel do Curso.dev. < /h1>
+  return <h1> Olá Galera, rodando deploy na Vercel do Curso.dev. </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
